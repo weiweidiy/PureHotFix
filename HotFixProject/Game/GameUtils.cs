f4796924a1be58d302062becafb665555f3b7bc5
weiweidiy <49595272@qq.com>
2022-07-23 16:36:52 +0800

    update

---
+++ HotFixProject/Game/GameUtils.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class GameUtils
+    {
+        /// <summary>
+        /// 平滑的进度值
+        /// </summary>
+        /// <param name="startOffset"></param>
+        /// <param name="duration"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+		public static float SmoothProgress(float startOffset, float duration, float time)
+        {
+            return Mathf.SmoothStep(0, 1, Progress(startOffset, duration, time));
+        }
+
+        /// <summary>
+        /// 获取进度
+        /// </summary>
+        /// <param name="startOffset"></param>
+        /// <param name="duration"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static float Progress(float startOffset, float duration, float time)
+        {
+            return Mathf.Clamp(time - startOffset, 0, duration) / duration;
+        }
+
+        /// <summary>
+        /// 转换成像素值
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="maxSize"></param>
+        /// <returns></returns>
+        public static int ToAbsoluteSize(float value, int maxSize)
+        {
+            if (value <= 1)
+            {
+                value = maxSize * value;
+            }
+
+            return (int)Mathf.Clamp(Mathf.Floor(value), 0, maxSize);
+        }
+    }
+}
---
+++ HotFixProject/Game/Launcher.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
+
+namespace Game
+{
+    public interface ITransition
+    {
+        /// <summary>
+        /// 开始转出：比如逐步黑屏等
+        /// </summary>
+        Task BeginOut();
+
+        /// <summary>
+        /// 开始转入
+        /// </summary>
+        Task BeginIn();
+    }
+
+    public class FadeTransition : ITransition
+    {
+        public Task BeginIn()
+        {
+
+        }
+
+        public Task BeginOut()
+        {
+
+        }
+    }
+
+    public class GameSceneManager
+    {
+        public async void LoadSceneAsync(string sceneName, LoadSceneMode mode, ITransition transition = null)
+        {
+            if (transition != null)
+            {
+                await transition.BeginOut();
+                Debug.Log("BeginOut Complete");
+            }
+
+            await Addressables.LoadSceneAsync(sceneName, mode).Task;
+
+            Debug.Log("Load Scene Complete " + sceneName);
+
+            if (transition != null)
+            {
+                await transition.BeginIn();
+                Debug.Log("BeginIn Complete");
+            }
+        }
+
+        public AsyncOperation LoadSceneAsync(string sceneName, ITransition transition = null)
+        {
+            return SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
+    public class Launcher
+    {
+        public void Launch()
+        {
+            Debug.Log("Launcher is Running");
+            //return null;
+            //加载核心程序集
+            //TextAsset asset = Addressables.LoadAssetAsync<TextAsset>("ObjectPool_dll").WaitForCompletion();
+            //return Assembly.Load(asset.bytes);
+
+            //return Assembly.Load(asset.bytes);
+
+
+
+            GameSceneManager sceneManager = new GameSceneManager();
+            sceneManager.LoadSceneAsync("Login", new )
+        }
+    }
+}
